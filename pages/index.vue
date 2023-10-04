fc8a6262c0f18cdc633aa314c51396eb4ba2e004
danix86 <daniel.daxer34@gmail.com>
2023-10-04 15:14:07 +0200

    dings

--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,14 @@ import { storeToRefs } from "pinia";
 
 const store = useStore();
 const jokes = storeToRefs(store).jokes;
-let advice = ref("");
-let satz = ref("");
+let cardText = ref("");
+let inputText = ref("");
 
 definePageMeta({
   alias: "/home",
 });
 
-onMounted(() => {
+onMounted(async () => {
   getChuck();
 });
 
@@ -20,32 +20,32 @@ async function getChuck() {
   const response = await fetch("https://api.chucknorris.io/jokes/random");
   let data = await response.json();
   if (data.value.length <= 250)
-    advice.value = "\"" + data.value + "\"";
+    cardText.value = "\"" + data.value + "\"";
 }
 
-function show(index) {
-  advice.value = jokes.value[index].name;
+function show(id) {
+  cardText.value = jokes.value.filter(joke => joke.id == id)[0].name;
 }
 
-function addChuck() {
+function addFav() {
   jokes.value.push({
     id: Date.now().toString(16),
-    name: advice.value
+    name: cardText.value
   });
 }
 
-function add() {
-  if (satz.value.length <= 250 && satz.value.length > 0) {
+function addInputToFav() {
+  if (inputText.value.length <= 250 && inputText.value.length > 0) {
     jokes.value.push({
       id: Date.now().toString(16),
-      name: "\"" + satz.value + "\""
+      name: "\"" + inputText.value + "\""
     });
-    satz.value = "";
+    inputText.value = "";
   }
 }
 
-function del(index) {
-  jokes.value.splice(index, 1);
+function del(id) {
+  jokes.value = jokes.value.filter(joke => joke.id != id);
 }
 
 function delAll() {
@@ -57,7 +57,7 @@ function delAll() {
 <template>
 
   <div class="dings" flex flex-col p-3 h-90 w-120 rounded-lg>
-    <h1 text-xl font-bold text-center class="card-body">{{ advice }}</h1>
+    <h1 text-xl font-bold text-center class="card-body">{{ cardText }}</h1>
     <div flex justify-center gap-1>
       <label h-18 rounded-full for="my-modal" class="btn btn-outline btn-info">
         <Icon text-8 icon="material-symbols:menu" />
@@ -65,7 +65,7 @@ function delAll() {
       <label h-18 rounded-full @click="getChuck" class="btn btn-outline btn-success">
         <Icon text-8 icon="ph:dice-five-fill" />
       </label>
-      <label h-18 rounded-full @click="addChuck" class="btn btn-outline btn-warning">
+      <label h-18 rounded-full @click="addFav" class="btn btn-outline btn-warning">
         <Icon text-8 icon="material-symbols:heart-plus" />
       </label>
     </div>
@@ -74,17 +74,17 @@ function delAll() {
   <input type="checkbox" id="my-modal" class="modal-toggle" />
   <div class="modal">
     <div class="modal-box">
-      <ul v-for="(joke, index) in jokes" :key="joke.id">
+      <ul v-for="joke in jokes" :key="joke.id">
         <label flex gap-4 items-center py-3 for="my-modal">
-          <label rounded-full class="btn btn-outline btn-error" @click="del(index)">X</label>
-          <p hover:bg-slate-700 active:bg-slate-800 w-full pl-2 pr-1 py-1 @click="show(index)">{{ joke.name }}</p>
+          <label rounded-full class="btn btn-outline btn-error" @click="del(joke.id)">X</label>
+          <p hover:bg-slate-700 active:bg-slate-800 w-full pl-2 pr-1 py-1 @click="show(joke.id)">{{ joke.name }}</p>
         </label>
       </ul>
       <div flex justify-between class="modal-action">
         <label @click="delAll" class="btn btn-outline btn-error">Delete All</label>
-        <input v-model="satz" @keyup.enter="add" placeholder="Write your joke" />
-        <label v-if="satz.length > 0 && satz.length <= 250">{{ satz.length }}</label>
-        <label text-red v-if="satz.length > 250">{{ satz.length }}</label>
+        <input v-model="inputText" @keyup.enter="addInputToFav" placeholder="Write your joke" />
+        <label v-if="inputText.length > 0 && inputText.length <= 250">{{ inputText.length }}</label>
+        <label text-red v-if="inputText.length > 250">{{ inputText.length }}</label>
         <label for="my-modal" class="btn">Leave</label>
       </div>
     </div>
